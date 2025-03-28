c8d3056d8351dd3c8c78ff84e2d4ad11fb7f9589
xiaoyi0814 <1954742838@qq.com>
2025-03-28 15:24:38 +0800

    feature: support RespFrame decode

---
+++ README.md
@@ -0,0 +1,9 @@
+# Redis-rs
+
+
+Using Tokio and Rust's futures to create an asynchronous Redis client.
+
+
+- RespFrame
+    - 使用RespEncoder/RespDecoder trait统一行为
+    - unit test
---
+++ src/resp/mod.rs
@@ -0,0 +1,203 @@
+/*
+- 如何 serialize/deserialize Frame
+    - simple string: "+OK\r\n"
+    - error: "-Error message\r\n"
+    - bulk error: "!<length>\r\n<error>\r\n"
+    - integer: ":[<+|->]<value>\r\n"
+    - bulk string: "$<length>\r\n<data>\r\n"
+    - null bulk string: "$-1\r\n"
+    - array: "*<number-of-elements>\r\n<element-1>...<element-n>"
+        - "*2\r\n$3\r\nget\r\n$5\r\nhello\r\n"
+    - null array: "*-1\r\n"
+    - null: "_\r\n"
+    - boolean: "#<t|f>\r\n"
+    - double: ",[<+|->]<integral>[.<fractional>][<E|e>[sign]<exponent>]\r\n"
+    - big number: "([+|-]<number>\r\n"
+    - map: "%<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>"
+    - set: "~<number-of-elements>\r\n<element-1>...<element-n>"
+*/
+
+mod array;
+mod bool;
+mod bulk_string;
+mod double;
+mod frame;
+mod integer;
+mod map;
+mod null;
+mod set;
+mod simple_error;
+mod simple_string;
+
+pub use self::{
+    array::{RespArray, RespNullArray},
+    bulk_string::{BulkString, RespNullBulkString},
+    frame::RespFrame,
+    map::RespMap,
+    null::RespNull,
+    set::RespSet,
+    simple_error::SimpleError,
+    simple_string::SimpleString,
+};
+use anyhow::Result;
+use bytes::{Buf, BytesMut};
+use enum_dispatch::enum_dispatch;
+use thiserror::Error;
+
+const BUF_CAP: usize = 4096;
+const CRLF: &[u8] = b"\r\n";
+const CRLF_LEN: usize = CRLF.len();
+
+#[enum_dispatch]
+pub trait RespEncode {
+    fn encode(self) -> Vec<u8>;
+}
+
+pub trait RespDecode: Sized {
+    const PREFIX: &'static str;
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError>;
+    fn expect_length(buf: &[u8]) -> Result<usize, RespError>;
+}
+
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum RespError {
+    #[error("invalid frame: {0}")]
+    InvalidFrame(String),
+    #[error("invalid frame type: {0}")]
+    InvalidFrameType(String),
+    #[error("invalid frame length: {0}")]
+    InvalidFrameLength(isize),
+    #[error("Frame is not complete")]
+    NotComplete,
+
+    #[error("Parse error: {0}")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("Utf-8 error: {0}")]
+    Utf8Error(#[from] std::string::FromUtf8Error),
+    #[error("Parse Float error: {0}")]
+    ParseFloatError(#[from] std::num::ParseFloatError),
+}
+
+/**
+ * - find the nth crlf in the buffer
+ */
+fn find_crlf(buf: &[u8], nth: usize) -> Option<usize> {
+    let mut count = 0;
+    for i in 1..buf.len() - 1 {
+        if buf[i] == b'\r' && buf[i + 1] == b'\n' {
+            count += 1;
+            if count == nth {
+                return Some(i);
+            }
+        }
+    }
+    None
+}
+
+fn parse_length(buf: &[u8], prefix: &str) -> Result<(usize, usize), RespError> {
+    let end = extract_simple_frame_data(buf, prefix)?;
+    let s = String::from_utf8_lossy(&buf[prefix.len()..end]);
+
+    Ok((end, s.parse()?))
+}
+
+/**
+ * - extract the fixed data of the frame
+ */
+fn extract_fixed_data(
+    buf: &mut BytesMut,
+    expect: &str,
+    expect_type: &str,
+) -> Result<(), RespError> {
+    if buf.len() < expect.len() {
+        return Err(RespError::NotComplete);
+    }
+
+    if !buf.starts_with(expect.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expect: {}, got: {:?}",
+            expect_type, buf
+        )));
+    }
+
+    buf.advance(expect.len());
+    Ok(())
+}
+
+/**
+ * - extract the data of the frame
+ *  - check the prefix
+ *  - find the end of the frame
+ */
+fn extract_simple_frame_data(buf: &[u8], prefix: &str) -> Result<usize, RespError> {
+    if buf.len() < 3 {
+        return Err(RespError::NotComplete);
+    }
+
+    println!("buf: {:?}", buf);
+    if !buf.starts_with(prefix.as_bytes()) {
+        return Err(RespError::InvalidFrameType(format!(
+            "expect: SimpleString({}), got: {:?}",
+            prefix, buf
+        )));
+    }
+
+    let end = find_crlf(buf, 1).ok_or(RespError::NotComplete)?;
+    Ok(end)
+}
+
+/**
+ * - calculate the total length of the frame
+ */
+fn calc_total_length(buf: &[u8], end: usize, len: usize, prefix: &str) -> Result<usize, RespError> {
+    let mut total_len = end + CRLF_LEN;
+    let mut data = &buf[total_len..];
+    match prefix {
+        "*" | "~" => {
+            // find nth CRLF in the buffer, for array and set, we need to find 1 CRLF for each element
+            for _ in 0..len {
+                let len = RespFrame::expect_length(data)?;
+                data = &data[len..];
+                total_len += len;
+            }
+
+            Ok(total_len)
+        }
+
+        "%" => {
+            // find nth CRLF in the buffer, for map, we need to find 2 CRLF for each key-value pair
+            // FixNote: In test 'test_map_decode', Error: invalid frame type: expect: SimpleString(+), got: [98, 97, 114, 13, 10, 43, 104, 101, 108, 108, 111, 13, 10, 36, 53, 13, 10, 119, 111, 114, 108, 100, 13, 10]
+            // The reason is that exchange the position of the key:'SimpleString' and value:'RespFrame' in the map
+            for _ in 0..len {
+                let len = SimpleString::expect_length(data)?;
+                data = &data[len..];
+                total_len += len;
+                let len = RespFrame::expect_length(data)?;
+                data = &data[len..];
+                total_len += len;
+            }
+
+            Ok(total_len)
+        }
+        _ => Ok(len + CRLF_LEN),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    #[test]
+    fn test_calc_array_length() -> Result<()> {
+        let buf = b"*2\r\n$3\r\nset\r\n$5\r\nhello\r\n";
+        let (end, len) = parse_length(buf, "*")?;
+        let total_len = calc_total_length(buf, end, len, "*")?;
+        assert_eq!(total_len, buf.len());
+
+        let buf = b"*2\r\n$3\r\nset\r\n";
+        let (end, len) = parse_length(buf, "*")?;
+        let ret = calc_total_length(buf, end, len, "*");
+        assert_eq!(ret.unwrap_err(), RespError::NotComplete);
+        Ok(())
+    }
+}
